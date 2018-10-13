0b86f6bc2e9758c336d355137f449a32638a5e3c
sasha merkulev <a_lexandr@mail.ru>
2018-10-13 19:23:53 +0300

    to kotlin

---
+++ app/src/main/java/ru/merkulyevsasha/news/newsjobs/RebootActionReceiver.kt
@@ -0,0 +1,19 @@
+package ru.merkulyevsasha.news.newsjobs
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class RebootActionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+
+        if (action != null && (action == Intent.ACTION_BOOT_COMPLETED
+                || action == "android.intent.action.QUICKBOOT_POWERON"
+                || action == "com.htc.intent.action.QUICKBOOT_POWERON")) {
+
+            NewsWorkerRunner().run()
+        }
+
+    }
+}
