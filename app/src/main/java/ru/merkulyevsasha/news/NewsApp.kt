3b19d431de9cfd63053ecc3e46b6d191fa24ac77
sasha merkulev <a_lexandr@mail.ru>
2019-05-20 04:23:14 +0300

    worker off

--- app/src/main/java/ru/merkulyevsasha/news/NewsApp.kt
+++ app/src/main/java/ru/merkulyevsasha/news/NewsApp.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import com.facebook.stetho.Stetho
 import com.google.android.gms.ads.MobileAds
 import ru.merkulyevsasha.ServiceLocator
-import ru.merkulyevsasha.news.newsjobs.NewsWorkerPeriodicRunner
 
 class NewsApp : Application() {
 
@@ -15,7 +14,7 @@ class NewsApp : Application() {
         serviceLocator = ServiceLocator(this)
 
         MobileAds.initialize(this, getString(R.string.APP_ID))
-        NewsWorkerPeriodicRunner().runWorker()
+//        NewsWorkerPeriodicRunner().runWorker()
 
         if (BuildConfig.DEBUG_MODE) {
             Stetho.initializeWithDefaults(this)
