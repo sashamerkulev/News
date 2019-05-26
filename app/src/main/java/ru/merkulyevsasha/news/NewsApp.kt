ac36a8e76c99b117bf82b264733a9952b1e5adfe
sasha merkulev <a_lexandr@mail.ru>
2019-05-26 04:02:47 +0300

    added leak canary

--- app/src/main/java/ru/merkulyevsasha/news/NewsApp.kt
+++ app/src/main/java/ru/merkulyevsasha/news/NewsApp.kt
@@ -3,6 +3,7 @@ package ru.merkulyevsasha.news
 import android.app.Application
 import com.facebook.stetho.Stetho
 import com.google.android.gms.ads.MobileAds
+import com.squareup.leakcanary.LeakCanary
 import ru.merkulyevsasha.ServiceLocator
 
 class NewsApp : Application() {
@@ -19,6 +20,13 @@ class NewsApp : Application() {
         if (BuildConfig.DEBUG_MODE) {
             Stetho.initializeWithDefaults(this)
         }
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
     fun getServiceLocator(): ServiceLocator {
