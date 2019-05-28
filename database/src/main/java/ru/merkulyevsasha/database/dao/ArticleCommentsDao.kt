0842ce50b91f8b4fbe4d3abe8d6588c2e3a484dc
sasha merkulev <a_lexandr@mail.ru>
2019-05-29 02:05:50 +0300

    added comments entitiy/dao

--- database/src/main/java/ru/merkulyevsasha/database/dao/ArticleCommentsDao.kt
+++ database/src/main/java/ru/merkulyevsasha/database/dao/ArticleCommentsDao.kt
@@ -1,12 +1,21 @@
 package ru.merkulyevsasha.database.dao
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
 import io.reactivex.Single
 import ru.merkulyevsasha.database.entities.ArticleCommentsEntity
 
 @Dao
 interface ArticleCommentsDao {
-    @Query("select * from comments order by date desc")
+    @Query("select * from comments order by pubDate desc")
     fun getArticleComments(): Single<List<ArticleCommentsEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrUpdate(items: List<ArticleCommentsEntity>)
+
+    @Update
+    fun update(item: ArticleCommentsEntity)
 }
--- database/src/main/java/ru/merkulyevsasha/database/entities/ArticleCommentsEntity.kt
+++ database/src/main/java/ru/merkulyevsasha/database/entities/ArticleCommentsEntity.kt
@@ -1,18 +1,26 @@
 package ru.merkulyevsasha.database.entities
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.Index
 import android.arch.persistence.room.PrimaryKey
-import java.util.Date
+import java.util.*
 
-@Entity(tableName = "comments", indices = [Index("articleId")])
+@Entity(tableName = "comments",
+    foreignKeys = [ForeignKey(entity = ArticleEntity::class, parentColumns = ["articleId"], childColumns = ["articleId"], onDelete = ForeignKey.CASCADE)],
+    indices = [Index("articleId"), Index("pubDate")])
 class ArticleCommentsEntity(
     @PrimaryKey
     val commentId: Int,
     val articleId: Int,
+    val userId: Int,
     val userName: String,
-    val userPhoto: String,
-    val date: Date,
+    val pubDate: Date,
     val comment: String,
-    val status: Int
+    val status: Int,
+    val likes: Int,
+    val dislikes: Int,
+    val like: Boolean,
+    val dislike: Boolean,
+    val owner: Boolean
 )
