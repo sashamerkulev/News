89445a1d5ab708291b98ac6eb21571b808fdb21b
sasha merkulev <a_lexandr@mail.ru>
2019-05-29 03:09:31 +0300

    added sort by last activity date

--- database/src/main/java/ru/merkulyevsasha/database/dao/ArticleDao.kt
+++ database/src/main/java/ru/merkulyevsasha/database/dao/ArticleDao.kt
@@ -11,10 +11,10 @@ import java.util.*
 
 @Dao
 interface ArticleDao {
-    @Query("select * from articles order by pubDate desc")
+    @Query("select * from articles order by lastActivityDate desc")
     fun getArticles(): Single<List<ArticleEntity>>
 
-    @Query("select * from articles where isUserLiked or isUserCommented or isUserDisliked order by pubDate desc")
+    @Query("select * from articles where isUserLiked or isUserCommented or isUserDisliked order by lastActivityDate desc")
     fun getUserActivityArticles(): Single<List<ArticleEntity>>
 
     @Query("delete from articles where pubDate < :cleanDate and lastActivityDate < :cleanDate and not (isUserLiked or isUserCommented or isUserDisliked)")
